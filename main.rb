9496da3808c9b1baddc66d3eb5919c3d09adbc78
camelia.moise2106 <camelia.moise21@gmail.com>
2016-10-23 15:49:19 +0300

    final commit

--- main.rb
+++ main.rb
@@ -1,19 +1,29 @@
-require 'koala'
+require          'koala'
 require_relative './config/secrets.rb'
 require_relative './lib/markov.rb'
 
 
+
 markov = Markov.new
 
-text = File.read('./sample_2')
+if File.exists?('./tmp/state')
+	dump   = File.read('./tmp/state')
+	markov = Marshal.load(dump)
+else
 
+	text = File.read('./sample_1')
+	markov.feed(text)
+	markov.normalize!
 
-markov.feed(text)
-markov.normalize!
+end
 
 post = markov.generate_phrase(', ')
-puts post
-
 api = Koala::Facebook::API.new(FB_APP_TOKEN)
-# api.put_wall_post(post)
+api.put_wall_post(post)
  
+unless File.exists?('./tmp/state')
+	File.open('./tmp/state', 'w') do |file|
+		Marshal.dump(markov, file)
+		end	
+end
+	

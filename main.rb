2ddc1e1fbc833a886e8b1dceed7729db605dff72
camelia.moise2106 <camelia.moise21@gmail.com>
2016-10-23 13:47:32 +0300

    generators

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
+require 'koala'
 require_relative './config/secrets.rb'
+require_relative './lib/markov.rb'
 
-puts FB_APP_TOKEN
+
+markov = Markov.new
+
+text = File.read('./sample_2')
+
+
+markov.feed(text)
+post = markov.generate_phrase(', ')
+puts post
+
+api = Koala::Facebook::API.new(FB_APP_TOKEN)
+# api.put_wall_post(post)
+ 
